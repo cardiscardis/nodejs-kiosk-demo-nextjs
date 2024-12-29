f4ad02fa1c8735bca1dfb6de10ff18f820646830
cardiscardis <giftedcardis@outlook.com>
2024-12-29 20:52:37 +0100

    node

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const public_key="04feeacb33942e28d53fb0a72417ca00f3664c51125b9e7bac4add41f6520709822a467878221b515a0aeb5bd86ee88328473d0da73017eb4996dffc4f71285d9b"
+/* To complete your setup, Go to https://test.bitpay.com/dashboard/merchant/api-tokens and pair this client with your merchant account using the pairing codes:
+JJtk6fL for the Merchant facade.
+Gi94oqu for the Payout facade ONLY if you have requested access for this role. */
